cda882e432c444f9574e340d2436081b04031f01
Reneginf <reneginf@gmail.com>
2022-09-12 14:36:25 +0000

    .

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = DomainGen();
+  console.log(DomainGen);
+};
+
+let DomainGen = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+
+  return pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".com";
 };
